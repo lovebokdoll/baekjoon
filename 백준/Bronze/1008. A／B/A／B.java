a2513b865569bf17771cbe4aae3ea7f822e596ed
lovebokdoll <92866090+lovebokdoll@users.noreply.github.com>
2023-06-11 21:11:51 +0900

    [Bronze V] Title: A/B, Time: 220 ms, Memory: 18008 KB -BaekjoonHub

---
+++ 백준/Bronze/1008. A／B/A／B.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Main{
+    public static void main(String[] args){
+        Scanner scan = new Scanner(System.in);
+        
+        double a =scan.nextDouble();
+        double b =scan.nextDouble();
+        
+        System.out.print(a/b);
+        
+        scan.close();
+    }
+}
